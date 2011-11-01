61d6e9dff90e9643ad90c011dccaf3873079f10b
Mitko Masarliev <mitko@masarliev.net>
2011-11-01 23:30:05 +0200

    management and examples

--- gitlog/management/commands/postreceive.py
+++ gitlog/management/commands/postreceive.py
@@ -8,10 +8,7 @@ from gitlog.models import Project, Push
 from gitlog import settings
 from git import Repo
 from django.contrib.auth.models import User
-import ConfigParser
-import io
-import random
-import string
+import ConfigParser, io, random, string
 class Command(BaseCommand):
     args = '<oldrev newrev refname>'
     help = 'Update project'
@@ -30,28 +27,15 @@ class Command(BaseCommand):
                         if option == 'writable' or option == 'readonly':
                             repos = items.split(' ')
                             for repo in repos:
-                                project, created = Project.objects.get_or_create(name=repo)
-                                if created:
-                                    if config.has_section('repo %s' % repo):
-                                        for option, items in config.items('repo %s' % repo):
-                                            if option == 'owner':
-                                                user, created = self.get_or_create_user(username=items.split(' ')[0])
-                                                project.owner = user
-                                                project.save()
-                                            if option == 'description':
-                                                project.description = items
-                                                project.save()
-                                            if option == 'url':
-                                                project.url = items
-                                                project.save()
-                                    self.stdout.write('Created project: "%s"\n' % repo)
-                                
-                                for type, items in config.items(section):
+                                if repo == 'gitosis-admin':
+                                    continue
+                                project, created = self.get_or_create_project(name=repo, config=config)
+                                for type, members in config.items(section):
                                     if type == 'members':
-                                        users = items.split(' ')
+                                        users = members.split(' ')
                                         for repouser in users:
                                             if repouser.startswith('@'):
-                                                user, created = self.get_or_create_user(repouser.replace('@', ''))
+                                                user, created = self.get_or_create_user(repouser.replace('@', ''), config=config)
                                                 if option == 'writable':
                                                     project.writable.add(user)
                                                 if option == 'readonly':
@@ -69,11 +53,37 @@ class Command(BaseCommand):
             push.newrev = newrev
             push.reference = refname
             push.save()
-    def get_or_create_user(self, username):
+    def get_or_create_user(self, username, config):
         user, created = User.objects.get_or_create(username=username)
         if created:
-            password = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(8))
-            user.set_password(password)
+            if config.has_section('user %s' % username):
+                for option, items in config.items('user %s' % username):
+                    if option == 'password':
+                        password = items
+                        user.set_password(items)
+                    if option == 'email':
+                        user.email = items
+            if not config.has_option('user %s' % username, 'password'):
+                password = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(8))
+                user.set_password(password)
             user.save()
             self.stdout.write('Created user: username "%s"; password "%s"\n' % (username, password))
         return user, created
+    
+    def get_or_create_project(self, name, config):
+        project, created = Project.objects.get_or_create(name=name)
+        if created:
+            if config.has_section('repo %s' % name):
+                for option, items in config.items('repo %s' % name):
+                    if option == 'owner':
+                        user, created = self.get_or_create_user(username=items.split(' ')[0], config=config)
+                        project.owner = user
+                        project.save()
+                    if option == 'description':
+                        project.description = items
+                        project.save()
+                    if option == 'url':
+                        project.url = items
+                        project.save()
+            self.stdout.write('Created project: "%s"\n' % name)
+        return project, created
